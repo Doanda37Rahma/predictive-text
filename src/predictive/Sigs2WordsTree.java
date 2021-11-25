67a52a8aa274a78eed0b08c20b92af720d8329d3
Zakyyy <73029787+AthaDzaky@users.noreply.github.com>
2021-11-25 15:36:46 +0700

    Update Sigs2WordsTree.java

--- src/predictive/Sigs2WordsTree.java
+++ src/predictive/Sigs2WordsTree.java
@@ -1,10 +1,12 @@
 package predictive;
+import java.util.Set;
+
+
 
 public class Sigs2WordsTree {
-	
 	public static void main(String[] args) {
 
-		Dictionary dict = new DictionaryTreeImpl();
+		Dictionary dict = new DictionaryTreeImpl("words.txt");
 
 		for (int i = 0; i < args.length; i++) {
 			System.out.println(args[i] + ": " + dict.signatureToWords(args[i]));
